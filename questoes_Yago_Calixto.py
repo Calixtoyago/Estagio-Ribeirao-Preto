0fb1625304da6e1ffd7ee0f175b6ea75e6182855
Yago Calixto <140445576+Calixtoyago@users.noreply.github.com>
2024-10-26 18:21:40 -0300

    Add files via upload

---
+++ questoes_Yago_Calixto.py
@@ -0,0 +1,43 @@
+# questao 1
+print('QUESTAO 1')
+def fibonacci(n):
+    a, b = 0, 1
+    while True:
+        if n == b or n == 0:
+            print('O numero faz parte da Sequencia de Fibonacci')
+            break
+        elif n < b:
+            print('O numero não faz parte da Sequencia de Fibonacci')
+            break
+        c = a + b
+        a, b = b, c
+
+numero = int(input('Insira um numero inteiro: '))
+fibonacci(numero)
+
+
+# questao 2
+print('\nQUESTAO 2')
+def verificar_string(letra, palavra):
+    contagem = 0
+    for caracter in palavra.lower():
+        if caracter == letra:
+            contagem += 1
+    if contagem == 0:
+        print(f'A letra {letra} não se encontra na string {palavra}')
+    else:
+        print(f'A letra {letra} aparece {contagem} vezes na string {palavra}')
+
+palavra = input('Digite algo: ')
+verificar_string('a', palavra)
+
+
+# questao 3
+print('\nQUESTAO 3')
+indice = 12
+soma = 0
+k = 1
+while k < indice:
+    k += 1
+    soma += k
+print(f'o resultado vai ser {soma}')
